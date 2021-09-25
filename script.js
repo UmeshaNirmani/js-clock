18fb55deaae931a43ffd11902c0a92376debd586
Umesha-Nirmani <58287574+Umesha-Nirmani@users.noreply.github.com>
2021-09-25 20:02:02 +0530

    added js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const clock = document.getElementById("clock");
 const hour = document.getElementById("hour");
 const min = document.getElementById("min");
 const sec = document.getElementById("sec");
 
+setInterval(() => {
+  let date = new Date();
+  let hours = (date.getHours() / 12) * 360 - 90;
+  let minute = (date.getMinutes() / 60) * 360 - 90;
+  let seconds = (date.getSeconds() / 60) * 360 - 90;
+  hour.style.transform = `rotate(${hours}deg)`;
+  min.style.transform = `rotate(${minute}deg)`;
+  sec.style.transform = `rotate(${seconds}deg)`;
+}, 1000);
